79e9fb6f68fe46a023337e9d4a31660d0d918a72
yabenatti <min.benatti@gmail.com>
2018-04-24 14:04:27 -0300

    Initial Commit

---
+++ ChatFirebaseSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatFirebaseSample
+//
+//  Created by Yasmin Benatti on 24/04/18.
+//  Copyright © 2018 Yasmin Benatti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
